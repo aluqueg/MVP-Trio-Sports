63185646ea20efae791fb90555e890690d9ff94c
alvaro luque <alvaroluque.g@gmail.com>
2024-08-13 10:13:17 +0200

    poner dos puntos en adminjs en enableUser

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,7 +4,7 @@ const adminController = require('../controllers/adminControllers')
 
 router.get('/', adminController.getAllUsers);
 router.put('/disableUser/:user_id', adminController.disableUser)
-router.put('/enableUser/user_id', adminController.enableUser)
+router.put('/enableUser/:user_id', adminController.enableUser)
 
 
 module.exports = router;
