b968bab5b79168e2089629e3e9a72b215965cac2
liu <alexmakigo@gmail.com>
2024-08-14 13:04:34 +0200

    formulario de registro completo

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,17 +1,12 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import { Register } from "../pages/Auth/Register/Register"
-import { Email } from "../pages/Auth/Register/Email"
-import { Name } from "../pages/Auth/Register/Name"
 
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/register' element={<Register/>}>
-          <Route index element={<Email/>}/>
-          <Route path="name" element={<Name/>}/>
-        </Route>
+        <Route path='/register' element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   )
