9d3c8749728b64dbc3177943c78d2ee0eacdece8
esteban <esteban13499@gmail.com>
2024-08-14 10:00:36 +0200

    eslint react/prop-types off and Container fluid Context Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,16 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppRoutes } from './routes/AppRoutes';
 import { TrioContextProvider } from './context/TrioContextProvider';
+import { Container } from 'react-bootstrap';
 
 function App() {
 
   return (
-    <>
-    <TrioContextProvider>
-      <AppRoutes/>
-    </TrioContextProvider>
-    </>
+    <Container fluid>
+      <TrioContextProvider>
+        <AppRoutes/>
+      </TrioContextProvider>
+    </Container>
   )
 }
 
