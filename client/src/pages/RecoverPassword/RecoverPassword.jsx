64c472630683434ba2c8a1cd2f7d61a3a476386f
alvaro luque <alvaroluque.g@gmail.com>
2024-08-22 12:19:41 +0200

    vista recuperar password y back sin nodemailer

--- client/src/pages/RecoverPassword/RecoverPassword.jsx
+++ client/src/pages/RecoverPassword/RecoverPassword.jsx
@@ -1,52 +1,57 @@
-import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import axios from 'axios'
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
 
 export const RecoverPassword = () => {
-
   const initialValue = {
     text: "",
-    show: false
-  }
+    show: false,
+  };
 
-  const [email, setEmail] = useState("")
-  const [msg, setMsg] = useState(initialValue)
+  const [email, setEmail] = useState("");
+  const [msg, setMsg] = useState(initialValue);
 
   const onSubmit = async () => {
-    if(!email){
-      setMsg({text: "El campo no puede estar vacío", show: true});
-      return
+    if (!email) {
+      setMsg({ text: "El campo no puede estar vacío", show: true });
+      return;
     }
-    try{
-      const res = await axios.post("http://localhost:4000/api/users/recoverPassword", email)
-    }catch(err){
-      console.log(err);      
+    try {
+      const res = await axios.post(
+        "http://localhost:4000/api/users/recoverPassword",
+        email
+      );
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   const handleChange = (e) => {
-    const email = e.target.value
-    setEmail(email)
-  }
+    const email = e.target.value;
+    setEmail(email);
+  };
 
   return (
-       <Form>
+    <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>¿Problemas con la contraseña?</Form.Label>
-        <Form.Control 
-          type="email" 
+        <Form.Control
+          type="email"
           placeholder="Enter email"
           name="email"
           value={email}
           onChange={handleChange}
-           />
-           {msg.show && <p>{msg.text}</p>}
-        <Form.Text>Introduce la dirección de correo electrónico que utilizas en tu cuenta. Te enviarémos instrucciones para reestablecer la contraseña.</Form.Text>
+        />
+        {msg.show && <p>{msg.text}</p>}
+        <Form.Text>
+          Introduce la dirección de correo electrónico que utilizas en tu
+          cuenta. Te enviarémos instrucciones para reestablecer la contraseña.
+        </Form.Text>
       </Form.Group>
       <Button variant="primary" onClick={onSubmit}>
         Submit
       </Button>
-      </Form>
-  )
-}
+    </Form>
+  );
+};
