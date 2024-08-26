fb6f9d3175cae23ae4a976e7b7b51dfe19df5045
esteban <esteban13499@gmail.com>
2024-08-26 12:10:32 +0200

    Edit user funcional

--- client/src/components/ModalEditUser/ModalEditUser.jsx
+++ client/src/components/ModalEditUser/ModalEditUser.jsx
@@ -5,7 +5,6 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { TrioContext } from "../../context/TrioContextProvider";
 import DatePicker from "react-datepicker";
-import { es } from "date-fns/locale/es";
 import { ModalCreateSport } from "../ModalCreateSport/ModalCreateSport";
 import axios from "axios";
 import * as formik from 'formik';
@@ -126,7 +125,7 @@ function ModalEditUser({ show, setShowModal, data }) {
   };
 
   const onEditSubmit = (values) => {
-    const date = format(startDate, "dd/MM/yyyy");
+    const date = format(startDate, "yyyy/MM/dd");
     const updatedEditUser = {
       ...editUser,
       ...values,
@@ -142,6 +141,7 @@ function ModalEditUser({ show, setShowModal, data }) {
       })
       .then((res) => {
         console.log(res);
+        handleClose();
       })
       .catch((err) => console.log(err));
   };
