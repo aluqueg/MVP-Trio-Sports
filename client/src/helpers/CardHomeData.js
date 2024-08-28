c62ccbc0acb2140da731e6e34e09aba647a3481f
esteban <esteban13499@gmail.com>
2024-08-28 16:02:10 +0200

    Maquetación del Home terminada

--- client/src/helpers/CardHomeData.js
+++ client/src/helpers/CardHomeData.js
@@ -1,6 +1,6 @@
 export const HOME_CARD_DATA=[
-  {title: "Cerca de tí", text: "Encuentra a personas cercanas que compartan tus deportes favoritos y conecta con ellas fácilmente."},
-  {title: "Pasión por el deporte", text: "Empieza hoy a conocer a personas apasionadas por el deporte. ¡Regístrate y forma parte de la comunidad deportiva más vibrante!"},
-  {title: "Organiza tu evento", text: "Organiza y participa en eventos deportivos locales. Desde torneos hasta encuentros casuales, siempre hay algo para ti."},
-  {title: "Conecta fácilmente", text: "“Habla con tus nuevos compañeros de deporte a través de nuestro sistema de mensajería instantánea."}
+  {img: "home1.jpg", title: "Cerca de tí", text: "Encuentra a personas cercanas que compartan tus deportes favoritos y conecta con ellas fácilmente."},
+  {img: "home2.jpg", title: "Pasión por el deporte", text: "Empieza hoy a conocer a personas apasionadas por el deporte. ¡Regístrate y forma parte de la comunidad deportiva más vibrante!"},
+  {img: "home3.jpg", title: "Organiza tu evento", text: "Organiza y participa en eventos deportivos locales. Desde torneos hasta encuentros casuales, siempre hay algo para ti."},
+  {img: "home4.jpg", title: "Conecta fácilmente", text: "“Habla con tus nuevos compañeros de deporte a través de nuestro sistema de mensajería instantánea."}
 ]
